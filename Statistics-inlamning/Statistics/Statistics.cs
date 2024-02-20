4bed078af1f4d495781da77b66f96075b2fd20c3
Erkaandersson <Erik.andersson.94@hotmail.com>
2024-02-20 19:33:22 +0100

    sum

---
+++ Statistics-inlamning/Statistics/Statistics.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+
+namespace Statistics
+{
+  
+    public class Statistics
+    {
+        public static int[] source = JsonConvert.DeserializeObject<int[]>(File.ReadAllText("data.json"));
+
+        public static dynamic DescriptiveStatistics()
+        {
+            Dictionary<string, dynamic> StatisticsList = new Dictionary<string, dynamic>()
+            {
+                { "Maximum", Maximum() },
+                { "Minimum", Minimum() },
+                { "Medelvärde", Mean() },
+                { "Median", Median() },
+                { "Typvärde", String.Join(", ", Mode()) }, 
+                { "Variationsbredd", Range() },
+                { "Standardavvikelse", StandardDeviation() }
+                
+            };
+
+            string output =
+                $"Maximum: {StatisticsList["Maximum"]}\n" +
+                $"Minimum: {StatisticsList["Minimum"]}\n" +
+                $"Medelvärde: {StatisticsList["Medelvärde"]}\n" +
+                $"Median: {StatisticsList["Median"]}\n" +
+                $"Typvärde: {StatisticsList["Typvärde"]}\n" +
+                $"Variationsbredd: {StatisticsList["Variationsbredd"]}\n" +
+                $"Standardavvikelse: {StatisticsList["Standardavvikelse"]}";
+
+            return output;
+        }
+
+        public static int Maximum()
+        {
+            Array.Sort(Statistics.source);
+            Array.Reverse(source);
+            int result = source[0];
+            return result;
+        }
+
+        public static int Minimum()
+        {
+            Array.Sort(Statistics.source);
+            int result = source[0];
+            return result;
+        }
+
+        public static double Mean()
+        { 
+            double total = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                total += source[i];
+            }
+            return total / source.Length;
+        }
+
+        public static double Median()
+        {
+            Array.Sort(source);
+            int size = source.Length;
+            int mid = size / 2;
+            int dbl = source[mid];
+            return dbl;
+        }
+
+        //En funktion med returntype array för att returnera ett array.
+        public static int[] Mode()
+        {
+            // Här går jag igenom source´arrayen för att räkna hur många gånger en siffra kommer upp.
+            //Sedan ordnas dom iordning. Så att det descendar i den ordningen som dom kommer upp oftast.
+            var groups =
+            source
+           .GroupBy(value => value)
+           .OrderByDescending(group => group.Count());
+
+            //Här tar jag alla element ifrån den första gruppen och tar samma antal som siffran kom upp, Och det är dess mode.
+           int modeCount = groups.First().Count();
+
+            int[] models =
+                groups
+                    .Where(group => group.Count() == modeCount)
+                    .Select(group => group.Key)
+                    .ToList().ToArray();
+
+            return models;
+        }
+
+        public static int Range()
+        {
+            Array.Sort(Statistics.source);
+            int min = source[0];
+            int max = source[0];
+
+            for (int i = 0; i < source.Length; i++)
+                if (source[i] > max)
+                    max = source[i];
+
+            int range = max - min;
+            return range;
+        }
+
+        public static double StandardDeviation() 
+        {
+
+            double average = source.Average();
+            double sumOfSquaresOfDifferences = source.Select(val => (val - average) * (val - average)).Sum();
+            double sd = Math.Sqrt(sumOfSquaresOfDifferences / source.Length);
+
+            double round = Math.Round(sd, 1);
+            return round;
+        }
+
+    }
+}
